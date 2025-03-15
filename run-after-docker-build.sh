6c66cebc85b04ed2a0610311e62d0f512e929b26
Diego <platechDev@gmail.com>
2025-03-15 06:11:46 -0500

    agregar configuraciones de nginx

--- run-after-docker-build.sh
+++
@@ -1,5 +0,0 @@
-# Habilitar el plugin STOMP
-docker exec rabbitmq-medfriend rabbitmq-plugins enable rabbitmq_stomp
-
-# Habilitar el plugin Web STOMP que permite conexiones WebSocket
-docker exec rabbitmq-medfriend rabbitmq-plugins enable rabbitmq_web_stomp
